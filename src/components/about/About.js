6901bb6f772ed65b7987b55cd7c29cb56db7de8d
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-05-24 00:52:50 -0500

    update about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./about.css";
 import ME from "../../assets/me-about.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 function About() {
   return (
@@ -10,10 +13,37 @@ function About() {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="" />
+            <img src={ME} alt="About Image" />
           </div>
         </div>
-        <div className="about__content"></div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>15 years</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>3</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>30+ completed projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+            voluptas fugiat sed nulla, qui soluta est aliquid vel ab praesentium
+            repellendus quidem! Ducimus, laboriosam aspernatur. Quas velit
+            distinctio officiis quia.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Contact Me
+          </a>
+        </div>
       </div>
     </section>
   );
