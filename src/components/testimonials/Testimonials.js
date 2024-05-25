ac57553976e9ed18d1a275920fecbde3e7b21a92
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-05-25 02:26:04 -0500

    update Testimonials component

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -5,61 +5,63 @@ import avatar2 from "../../assets/avatar2.png";
 import avatar3 from "../../assets/avatar3.png";
 import avatar4 from "../../assets/avatar4.png";
 
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: avatar1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magni corporis at culpa in harum fugit numquam, incidunt dignissimos placeat nulla animi rerum distinctio? Porro rem pariatur eos assumenda recusandae."
+  },
+  {
+    avatar: avatar2,
+    name: "Shatta Wale",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magni corporis at culpa in harum fugit numquam, incidunt dignissimos placeat nulla animi rerum distinctio? Porro rem pariatur eos assumenda recusandae."
+  },
+  {
+    avatar: avatar3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magni corporis at culpa in harum fugit numquam, incidunt dignissimos placeat nulla animi rerum distinctio? Porro rem pariatur eos assumenda recusandae."
+  },
+  {
+    avatar: avatar4,
+    name: "John Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magni corporis at culpa in harum fugit numquam, incidunt dignissimos placeat nulla animi rerum distinctio? Porro rem pariatur eos assumenda recusandae."
+  }
+];
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Reviews from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar1} alt="avatar1" />
-            <h5 className="client__name">John Smith</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              voluptates ratione consequatur eligendi ipsum facere quidem in
-              error veritatis aperiam quae doloremque praesentium quibusdam
-              totam, expedita eveniet? Corporis, temporibus repellat?
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar2} alt="avatar2" />
-            <h5 className="client__name">John Smith</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              voluptates ratione consequatur eligendi ipsum facere quidem in
-              error veritatis aperiam quae doloremque praesentium quibusdam
-              totam, expedita eveniet? Corporis, temporibus repellat?
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar3} alt="avatar3" />
-            <h5 className="client__name">John Smith</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              voluptates ratione consequatur eligendi ipsum facere quidem in
-              error veritatis aperiam quae doloremque praesentium quibusdam
-              totam, expedita eveniet? Corporis, temporibus repellat?
-            </small>
-          </div>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar4} alt="avatar4" />
-            <h5 className="client__name">John Smith</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-              voluptates ratione consequatur eligendi ipsum facere quidem in
-              error veritatis aperiam quae doloremque praesentium quibusdam
-              totam, expedita eveniet? Corporis, temporibus repellat?
-            </small>
-          </div>
-        </article>
-      </div>
+      <Swiper
+        className="container testimonials__container"
+        modules={[Navigation, Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
